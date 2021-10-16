23703e4ea588d3d2a7b03504952a15fe3276d1bd
oxalica <oxalicc@pm.me>
2021-10-16 08:50:16 +0800

    Add injections for manual comment indicator

--- queries/nvim-injections.scm
+++ queries/nvim-injections.scm
@@ -44,6 +44,72 @@
         (#match? @_func "(^|\\.)runCommand(CC|NoCC|Local|NoCCLocal)?$"))))
   argument: (indented_string) @bash)
 
+; Manually marked with an indicator comment
+; FIXME: Cannot dynamic inject before `#offset!` issue being resolved.
+; See: https://github.com/neovim/neovim/issues/16032
+
+; Using `#set!` inside `[]` doesn't work, so we need to split these queries.
+(
+  ((comment) @_language (#any-of? @_language "# bash" "/* bash */") (#set! "language" "bash")) .
+  [
+    ((indented_string) @content)
+    (bind
+      expression: [
+        (indented_string) @content
+        (binary (indented_string) @content)
+        (app argument: (indented_string) @content)])]
+  (#offset! @content 0 2 0 -2))
+(
+  ((comment) @_language (#any-of? @_language "# fish" "/* fish */") (#set! "language" "fish")) .
+  [
+    ((indented_string) @content)
+    (bind
+      expression: [
+        (indented_string) @content
+        (binary (indented_string) @content)
+        (app argument: (indented_string) @content)])]
+  (#offset! @content 0 2 0 -2))
+(
+  ((comment) @_language (#any-of? @_language "# vim" "/* vim */") (#set! "language" "vim")) .
+  [
+    ((indented_string) @content)
+    (bind
+      expression: [
+        (indented_string) @content
+        (binary (indented_string) @content)
+        (app argument: (indented_string) @content)])]
+  (#offset! @content 0 2 0 -2))
+(
+  ((comment) @_language (#any-of? @_language "# tmux" "/* tmux */") (#set! "language" "tmux")) .
+  [
+    ((indented_string) @content)
+    (bind
+      expression: [
+        (indented_string) @content
+        (binary (indented_string) @content)
+        (app argument: (indented_string) @content)])]
+  (#offset! @content 0 2 0 -2))
+(
+  ((comment) @_language (#any-of? @_language "# toml" "/* toml */") (#set! "language" "toml")) .
+  [
+    ((indented_string) @content)
+    (bind
+      expression: [
+        (indented_string) @content
+        (binary (indented_string) @content)
+        (app argument: (indented_string) @content)])]
+  (#offset! @content 0 2 0 -2))
+(
+  ((comment) @_language (#any-of? @_language "# yaml" "/* yaml */") (#set! "language" "yaml")) .
+  [
+    ((indented_string) @content)
+    (bind
+      expression: [
+        (indented_string) @content
+        (binary (indented_string) @content)
+        (app argument: (indented_string) @content)])]
+  (#offset! @content 0 2 0 -2))
+
 ; Reverse inject interpolation to override other injected languages.
 ; I cannot find other way to correctly highlight interpolations inside injected string.
 ; Related: https://github.com/nvim-treesitter/nvim-treesitter/issues/1688
